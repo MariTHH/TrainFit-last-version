89819821a76edcdec46fc9c3f8c4a03416bf3ca4
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-10-28 20:02:23 +0300

    create profile. close issue #7

--- ProjectSite-front/src/profile/script.jsx
+++ ProjectSite-front/src/profile/script.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppContainer from "../components/appContainer/script";
 import img from "../profile/img.png";
 import MainPage from "routes/mainPage/script";
@@ -12,13 +12,25 @@ function Profile() {
     const goBack = () => {
         window.history.back();
     }
-    React.useEffect(() => {
-        window.history.pushState(null, null, window.location.href);
-        window.onpopstate = () => {
-            window.history.go(1);
-        };
-    }, []);
+    // React.useEffect(() => {
+    //     window.history.pushState(null, null, window.location.href);
+    //     window.onpopstate = () => {
+    //         window.history.go(1);
+    //     };
+    // }, []);
 
+    function viewDiv(){
+        document.getElementById("profileBox").style.display = "block";
+    }
+    function view(){
+        document.getElementById("profileBox").style.display = "none";
+    }
+    const handleMouseClick = () => {
+        viewDiv()
+    }
+    const haa = () => {
+        view()
+    }
     return (
         <AppContainer>
             <div className="header1">
@@ -32,20 +44,37 @@ function Profile() {
                     <img className="user-info-avatar" src={img} alt="avatar"/>
                 </div>
                 <div className="list">
-                    <div className="profile">
+                    <div className="profile" onClick={() => handleMouseClick()}>
                         <a>Профиль</a>
                     </div>
-                    <div className="shedule">
+                    <div className="shedule" onClick={() => haa()}>
                         <a>Расписание</a>
                     </div>
                 </div>
             </div>
             <div className="graph"></div>
             <div className="shed"></div>
-            <div id="back-button">
-                <button className='pointer' id='prevResult' onClick={() => navigate("/")} type="reset">
-                    Back
-                </button>
+            <div className="back-button" onClick={() => navigate(-1)}>
+                <a>Back</a>
+            </div>
+
+            <div className="profileBox" id={"profileBox"}>
+                <input className="username" type="text" placeholder="username"/>
+                <input className="weight" type="text" placeholder="weight"/>
+                <div className="sex" id="r_select">
+                    <label>
+                        <input className="sex-radio" type="radio" name="sex_value" value="women"></input>
+                        <span>  Female </span>
+                    </label>
+                    <label>
+                        <input className="sex-radio" type="radio" name="sex_value" value="man"></input>
+                        <span>  Male </span>
+                    </label>
+                </div>
+                <div className="save">
+                    <a>Save</a>
+                </div>
+
             </div>
         </AppContainer>
     );
